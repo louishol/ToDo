b3136b2fe831242a0998d5118d5f027ba82f7b1c
louishol <louis_hol@hotmail.com>
2015-03-01 13:25:34 +0100

    Db werkend

--- ToDo/www/js/database.js
+++ ToDo/www/js/database.js
@@ -1,13 +1,16 @@
 
 var db;
 
-document.addEventListener('deviceready', onDeviceReady, false);
+$(document).ready(function()
+{
+    document.addEventListener('deviceready', onDeviceReady, false);
+    alert("document ready");
+});
 
 
 function onDeviceReady(){
     alert("Device ready");
     initializeDatabase();
-    $('#btnSubmit').on("click", btnSubmitClicked);
 }
 
 
@@ -29,52 +32,63 @@ function btnSubmitClicked(){
 
 function initializeDatabase() {
     db = window.openDatabase("Database", "1.0", "Demo", 2 * 1024 * 1024);
-
     db.transaction(function(trans)
     { 
         trans.executeSql('CREATE TABLE IF NOT EXISTS ToDo (name, description, time)');
 
-        alert("table aangemaakt");
+        //addToDo("Android maken", "Fragmenten voor android maken", "2uur");
         getAllToDos(trans);
-        alert("Alles opgehaald");
     }, errorCB);
 }
 
 function errorCB(err) {
     alert("Error processing SQL: " + err.message);
-
     return true;
 }
 
 function getAllToDos(trans) {
     trans.executeSql('SELECT * FROM ToDo', [], function(trans, results){
-        // var htmlRows = '';
-        // var len = results.rows.length;
-        // var settings = JSON.parse(localStorage.getItem("settings"));
-        // $("#todo").clear();
+        var len = results.rows.length;
+        alert(len);
 
-        // alert("len " : len + " settings " + settings);
-        // for (var i=0; i<len; i++){
-        //     var newtodo = "Naam : " + results.rows.item(i).name;
-
-
-        //     if(settings.ShowDescription === "on")
-        //     {
-        //         newtodo += " | Beschrijving : " + results.rows.item(i).description;
-        //     }
-        //     if(settings.ShowTime === "on")
-        //     {
-        //         newtodo += "| Om " + results.rows.item(i).time;
-        //     }
-        //     $("#todo").append("<p>" + newtodo + "</p>");
+        for(var i =0; i<len; i++)
+        {
+            alert(results.rows.item(i).name);
         }
     }, errorCB);
 }
 
+
+// function getAllToDos(trans) {
+//     trans.executeSql('SELECT * FROM ToDo', [], function(trans, results){
+//         // var htmlRows = '';
+//         // var len = results.rows.length;
+//         // var settings = JSON.parse(localStorage.getItem("settings"));
+//         // $("#todo").clear();
+
+//         // alert("len " : len + " settings " + settings);
+//         // for (var i=0; i<len; i++){
+//         //     var newtodo = "Naam : " + results.rows.item(i).name;
+
+
+//         //     if(settings.ShowDescription === "on")
+//         //     {
+//         //         newtodo += " | Beschrijving : " + results.rows.item(i).description;
+//         //     }
+//         //     if(settings.ShowTime === "on")
+//         //     {
+//         //         newtodo += "| Om " + results.rows.item(i).time;
+//         //     }
+//         //     $("#todo").append("<p>" + newtodo + "</p>");
+//         }
+//     }, errorCB);
+// }
+
+
 function addToDo(name, description, time){
     db.transaction(function(trans){ 
-        trans.executeSql('INSERT INTO ToDo (name, description, time) VALUES (?,?)', [ name, description, time ]);
-        getAllToDos(trans);
+        trans.executeSql('INSERT INTO ToDo (name, description, time) VALUES (?,?,?)', [ name, description, time ]);
+        alert("Ja");
     }, errorCB);
 }
 
